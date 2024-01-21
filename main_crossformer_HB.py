ee13cdfcc456d1dcb05f08d10ab6fbac2204cd00
amos <506474841@qq.com>
2024-01-21 14:32:01 +0800

    修复运行默认参数

--- main_crossformer_HB.py
+++ main_crossformer_HB.py
@@ -8,7 +8,7 @@ from utils.tools import string_split
 parser = argparse.ArgumentParser(description='CrossFormer')
 
 parser.add_argument('--data', type=str,  default='FW3.sqlite', help='data')
-parser.add_argument('--data_path', type=str, default='./FW3.sqlite', help='data file')  
+parser.add_argument('--data_path', type=str, default='./datasets/FW3.sqlite', help='data file')  
 parser.add_argument('--data_split', type=str, default='0.7,0.3',help='train/val/test split, can be ratio or number')
 parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location to store model checkpoints')
 
@@ -18,7 +18,7 @@ parser.add_argument('--seg_len', type=int, default=4, help='segment length (L_se
 parser.add_argument('--win_size', type=int, default=1, help='window size for segment merge')
 parser.add_argument('--factor', type=int, default=10, help='num of routers in Cross-Dimension Stage of TSA (c)')
 
-parser.add_argument('--data_dim', type=int, default=4, help='Number of dimensions of the MTS data (D)')
+parser.add_argument('--data_dim', type=int, default=6, help='Number of dimensions of the MTS data (D)')
 parser.add_argument('--d_model', type=int, default=512, help='dimension of hidden states (d_model)')
 parser.add_argument('--d_ff', type=int, default=1024, help='dimension of MLP in transformer')
 parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
@@ -28,7 +28,7 @@ parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
 parser.add_argument('--baseline', action='store_true', help='whether to use mean of past series as baseline for prediction', default=False)
 
 parser.add_argument('--num_workers', type=int, default=8, help='data loader num workers')
-parser.add_argument('--batch_size', type=int, default=128, help='batch size of train input data')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size of train input data')
 parser.add_argument('--train_epochs', type=int, default=6, help='train epochs')
 parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
 parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer initial learning rate')
